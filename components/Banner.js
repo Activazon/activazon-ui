4efb28b814fd5015b0faecfe632bf5bba601a82a
Kirk Douglas Jr <kirkodouglasjr@gmail.com>
2022-12-27 21:06:07 -0600

    added localization

--- components/Banner.js
+++ components/Banner.js
@@ -1,20 +1,28 @@
-const Banner = ({ title, description, showSearch }) => (
-  <div className="banner">
-    <div className="container">
-      <p className="banner-text banner-title">{title}</p>
-      {description && (
-        <p className="banner-text banner-description">{description}</p>
-      )}
-      {showSearch && (
-        <div className="search-form rounded">
-          <input type="text" placeholder="Search for your neighbourhood" />
-          <button type="submit" className="btn">
-            <i className="bi bi-search"></i>
-          </button>
-        </div>
-      )}
+import { useTrans } from "../lib/trans";
+
+const Banner = ({ title, description, showSearch }) => {
+  const { i } = useTrans();
+  return (
+    <div className="banner">
+      <div className="container">
+        <p className="banner-text banner-title">{title}</p>
+        {description && (
+          <p className="banner-text banner-description">{description}</p>
+        )}
+        {showSearch && (
+          <div className="search-form rounded">
+            <input
+              type="text"
+              placeholder={i("Search for your neighbourhood")}
+            />
+            <button type="submit" className="btn">
+              <i className="bi bi-search"></i>
+            </button>
+          </div>
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Banner;
