847c5622b70a76f356a546882737545582d7341e
Kirk Douglas Jr <kirkodouglasjr@gmail.com>
2022-12-29 12:05:40 -0600

    added meta tags

--- components/Head.js
+++ components/Head.js
@@ -1,19 +1,40 @@
 import Head from "next/head";
+import { useTrans } from "../lib/trans";
 
 const TITLE = "Activazon";
 
-const AppHead = ({ title }) => (
-  <Head>
-    <title>{title ? `${title} - ${TITLE}` : TITLE}</title>
-    <meta
-      name="description"
-      content="AI-powered insights for your neighborhood"
-    />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" href="/favicon.ico" />
-    <link rel="alternate" hrefLang="en-US" href="https://activazon.com/en-US" />
-    <link rel="alternate" hrefLang="es-ES" href="https://activazon.com/es-ES" />
-  </Head>
-);
+const AppHead = ({ title }) => {
+  const { i } = useTrans();
+  const titleText = title ? `${title} - ${TITLE}` : TITLE;
+  const descriptionText = i("AI-powered insights for your neighborhood");
+  return (
+    <Head>
+      <title>{titleText}</title>
+      <meta name="description" content={descriptionText} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+      <link
+        rel="alternate"
+        hrefLang="en-US"
+        href="https://activazon.com/en-US"
+      />
+      <link
+        rel="alternate"
+        hrefLang="es-ES"
+        href="https://activazon.com/es-ES"
+      />
+      {/* Meta tags */}
+      <meta property="og:site_name" content={TITLE} />
+      <meta property="og:title" content={titleText} />
+      <meta property="og:description" content={descriptionText} />
+      <meta
+        property="og:image"
+        itemprop="image"
+        content="https://www.activazon.com/thumb.jpg"
+      />
+      <meta property="og:type" content="website" />
+    </Head>
+  );
+};
 
 export default AppHead;
