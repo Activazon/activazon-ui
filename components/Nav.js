d2f7bfcd00b6c14d09c8f34bc1ab81e29ab4924f
Kirk Douglas Jr <kirkodouglasjr@gmail.com>
2022-12-29 18:53:29 -0600

    added locale change button

--- components/Nav.js
+++ components/Nav.js
@@ -1,20 +1,42 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
-const Nav = ({ pageTitle, backUrl }) => (
-  <nav className="navbar sticky-top">
-    <div className="container">
-      <Link className="navbar-brand" href={backUrl || "/"}>
-        {!backUrl ? (
-          <i className="bi bi-activity"></i>
-        ) : (
-          <i className="bi bi-caret-left-fill"></i>
-        )}{" "}
-        Activazon
-      </Link>
+const Nav = ({ pageTitle, backUrl }) => {
+  const router = useRouter();
+  const localButtonLabel = router.locale === "en-US" ? "ES" : "EN";
+  const toggleLocale = () => {
+    // switch to the other locale
+    const nextLocale = router.locale === "en-US" ? "es-ES" : "en-US";
+    router.push(
+      { pathname: router.pathname, query: router.query },
+      router.asPath,
+      {
+        locale: nextLocale,
+      }
+    );
+  };
 
-      <p className="page-title">{pageTitle}</p>
-    </div>
-  </nav>
-);
+  return (
+    <nav className="navbar sticky-top">
+      <div className="container">
+        <Link className="navbar-brand" href={backUrl || "/"}>
+          {!backUrl ? (
+            <i className="bi bi-activity"></i>
+          ) : (
+            <i className="bi bi-caret-left-fill"></i>
+          )}{" "}
+          Activazon
+        </Link>
+
+        <p className="page-title">{pageTitle}</p>
+        <div className="flex-grow-1"></div>
+        <button className="btn btn-locale-change" onClick={toggleLocale}>
+          {" "}
+          <i className="bi bi-translate" /> {localButtonLabel}
+        </button>
+      </div>
+    </nav>
+  );
+};
 
 export default Nav;
