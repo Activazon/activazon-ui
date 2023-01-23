a16fe9064b578b3e7446927a4eebcc06cb3f31f0
Kirk Douglas Jr <kirkodouglasjr@gmail.com>
2023-01-22 21:29:57 -0600

    partial login screen

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,34 +1,45 @@
+import { authSignIn } from "lib/api-v2";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+
 export const authOptions = {
-  // Configure one or more authentication providers
+  secret: process.env.AUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
   providers: [
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. "Sign in with...")
       name: "Credentials",
-      // `credentials` is used to generate a form on the sign in page.
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
-        email: { label: "Email Address", type: "text" },
+        username: { label: "username", type: "text" },
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials, req) {
-        // Add logic here to look up the user from the credentials supplied
-        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
+        const { username, password } = credentials;
+        const response = await authSignIn(username, password);
 
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user;
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
+        if (response.error || !response.pk || !response.access) {
           return null;
-
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
         }
+
+        return response;
       },
     }),
   ],
+  callbacks: {
+    async session({ session, user, token }) {
+      session.user = token.user;
+      return session;
+    },
+    async jwt({ token, user }) {
+      return {
+        ...token,
+        id: token.id || user.pk,
+        refresh_token: token.refresh_token || user.refresh.token,
+        access_token: token.access_token || user.access.token,
+        user: token.user || user,
+      };
+    },
+  },
 };
 export default NextAuth(authOptions);
