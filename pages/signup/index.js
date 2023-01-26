1ed811bf305b4170352d9394c495d52b61622197
Kirk Douglas Jr <kirkodouglasjr@gmail.com>
2023-01-26 15:18:30 -0600

    Update index.js

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -100,7 +100,7 @@ const Page = ({}) => {
                   <div>
                     <button
                       type="submit"
-                      className="btn btn-primary w-100 btn-lg fw-bolder"
+                      className="btn btn-cta w-100 btn-lg fw-bolder"
                       id="submit"
                     >
                       Sign Up
