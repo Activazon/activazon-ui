6b022b5c32655663676227a7261c0687b5216440
Kirk Douglas Jr <kirkodouglasjr@gmail.com>
2023-01-26 14:18:15 -0600

    redesigned  navigation

--- components/Nav.js
+++ components/Nav.js
@@ -4,93 +4,43 @@ import { useRouter } from "next/router";
 import { trackLocaleChange } from "lib/track";
 import { explorePath } from "lib/urls";
 import { useSession, signOut } from "next-auth/react";
+import NavMenu from "./NavMenu";
 
 const Nav = ({ title, backHref }) => {
-  const { status } = useSession();
-
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const router = useRouter();
-  const localButtonLabel =
-    router.locale === "en" ? "Verlo en Español" : "View in English";
-  const isAuthenticated = status === "authenticated";
-  const toggleLocale = () => {
-    // switch to the other locale
-    const nextLocale = router.locale === "en" ? "es" : "en";
-    trackLocaleChange(nextLocale);
-    router.push(
-      { pathname: router.pathname, query: router.query },
-      router.asPath,
-      {
-        locale: nextLocale,
-      }
-    );
-  };
 
   return (
-    <nav className="navbar sticky-top">
-      <div className="container">
-        <Link className="navbar-brand" href={backHref || explorePath()}>
-          {!backHref ? (
-            <i className="bi bi-activity"></i>
-          ) : (
-            <i className="bi bi-chevron-left"></i>
-          )}{" "}
-          {title || "Activazon"}
-        </Link>
+    <div className="sticky-top">
+      <nav className="navbar">
+        <div className="container">
+          <Link className="navbar-brand" href={backHref || explorePath()}>
+            {!backHref ? (
+              <i className="bi bi-activity"></i>
+            ) : (
+              <i className="bi bi-chevron-left"></i>
+            )}{" "}
+            {title || "Activazon"}
+          </Link>
 
-        <div className="flex-grow-1"></div>
-        {/* <button className="btn btn-locale-change" onClick={toggleLocale}>
+          <div className="flex-grow-1"></div>
+          {/* <button className="btn btn-locale-change" onClick={toggleLocale}>
           <i className="bi bi-translate" /> {localButtonLabel}
         </button> */}
-        <button
-          className="btn btn-nav-menu"
-          onClick={() => setIsMenuOpen(!isMenuOpen)}
-        >
-          {isMenuOpen ? (
-            <i className="bi bi-x-lg"></i>
-          ) : (
-            <i className="bi bi-list" />
-          )}
-        </button>
-      </div>
-      <div className={`navigation-overlay${isMenuOpen ? " open" : ""}`}>
-        <div className="container text-center">
-          <div className="row mt-3 gy-3">
-            <div className="col-12">
-              <Link href="/" className="btn btn-menu w-100">
-                <i className="bi bi-house-door-fill me-3" />
-                Explore
-              </Link>
-            </div>
-
-            {!isAuthenticated && (
-              <div className="col-12">
-                <Link href="/signin" className="btn btn-menu w-100">
-                  <i className="bi bi-person-circle me-3" />
-                  Sing In
-                </Link>
-              </div>
+          <button
+            className="btn btn-nav-menu"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          >
+            {isMenuOpen ? (
+              <i className="bi bi-x-lg"></i>
+            ) : (
+              <i className="bi bi-list" />
             )}
-
-            <div className="col-12">
-              <button onClick={toggleLocale} className="btn btn-menu w-100">
-                <i className="bi bi-translate me-3" />
-                {localButtonLabel}
-              </button>
-            </div>
-
-            {isAuthenticated && (
-              <div className="col-12">
-                <button onClick={signOut} className="btn btn-menu w-100">
-                  <i className="bi bi-person-circle me-3" />
-                  Sing Out
-                </button>
-              </div>
-            )}
-          </div>
+          </button>
         </div>
-      </div>
-    </nav>
+      </nav>
+      <NavMenu open={isMenuOpen} close={() => setIsMenuOpen(false)} />
+    </div>
   );
 };
 
---
+++ components/NavMenu.js
@@ -0,0 +1,102 @@
+import { useCallback } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { useSession } from "next-auth/react";
+
+const NavMenuItem = ({ icon, label, active, href, onClick }) => {
+  return (
+    <Link
+      class={`nav-link${active ? " active" : ""}`}
+      id="v-pills-home-tab"
+      data-toggle="pill"
+      href={href}
+      role="tab"
+      aria-controls="v-pills-home"
+      aria-selected="true"
+      onClick={onClick}
+    >
+      <p>
+        {icon}
+        {label}
+      </p>
+    </Link>
+  );
+};
+
+const NavMenu = ({ open, close }) => {
+  const router = useRouter();
+  const { status } = useSession();
+  const isAuthenticated = status === "authenticated";
+  const localButtonLabel =
+    router.locale === "en"
+      ? "Cambiar idioma a Español"
+      : "Change Language to English";
+  const toggleLocale = () => {
+    // switch to the other locale
+    const nextLocale = router.locale === "en" ? "es" : "en";
+    // trackLocaleChange(nextLocale);
+    router.push(
+      { pathname: router.pathname, query: router.query },
+      router.asPath,
+      {
+        locale: nextLocale,
+      }
+    );
+  };
+  const isActive = useCallback(
+    (path) => {
+      return path === router.pathname;
+    },
+    [router.pathname]
+  );
+
+  if (!open) {
+    return null;
+  }
+
+  return (
+    <>
+      <div
+        className="nav nav-menu-dropdown flex-column nav-pills"
+        id="v-pills-tab"
+        role="tablist"
+        aria-orientation="vertical"
+      >
+        <div className="container">
+          <NavMenuItem
+            icon={<i className="bi bi-binoculars me-3" />}
+            label="Explore"
+            active={isActive("/explore")}
+            href="/explore"
+          />
+          {!isAuthenticated && (
+            <NavMenuItem
+              icon={<i className="bi bi-box-arrow-in-right me-3" />}
+              label="Sign Up"
+              active={isActive("/signup")}
+              href="/signup"
+            />
+          )}
+          <NavMenuItem
+            icon={<i className="bi bi-translate me-3" />}
+            label={localButtonLabel}
+            href="#"
+            active={false}
+            onClick={toggleLocale}
+          />
+          {isAuthenticated && (
+            <NavMenuItem
+              icon={<i className="bi bi-box-arrow-left me-3" />}
+              label="Sign Out"
+              active={isActive("/signout")}
+              href="/signout"
+            />
+          )}
+        </div>
+      </div>
+      <div className="nav-overlay-fill" onClick={close} />
+    </>
+  );
+};
+
+export default NavMenu;
