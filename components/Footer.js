9cfa713ace2ccd5ff9f5d6706736a12960b85ddd
Kirk Douglas Jr <kirkodouglasjr@gmail.com>
2023-01-27 16:47:51 -0600

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,21 @@
 import Link from "next/link";
 import { useTrans } from "../lib/trans";
 
+const ProductHunt = () => (
+  <a
+    href="https://www.producthunt.com/posts/activazon?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-activazon"
+    target="_blank"
+  >
+    <img
+      src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=376912&theme=light"
+      alt="Activazon - AI&#0032;crime&#0032;analytics&#0032;for&#0032;travellers&#0032;and&#0032;residence | Product Hunt"
+      style="width: 250px; height: 54px;"
+      width="250"
+      height="54"
+    />
+  </a>
+);
+
 const Footer = () => {
   const { t } = useTrans();
   return (
@@ -19,6 +34,9 @@ const Footer = () => {
         >
           <i className="bi bi-instagram"></i> Instagram
         </Link>
+      </p>
+      <p>
+        <ProductHunt />
       </p>
     </div>
   );
