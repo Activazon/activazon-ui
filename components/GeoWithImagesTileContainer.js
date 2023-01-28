d993af73cacc518711a3f5edcd8747f13aeaae62
Kirk Douglas Jr <kirkodouglasjr@gmail.com>
2023-01-28 10:49:22 -0600

    Update GeoWithImagesTileContainer.js

--- components/GeoWithImagesTileContainer.js
+++ components/GeoWithImagesTileContainer.js
@@ -1,5 +1,5 @@
 const GeoWithImageTileContainer = ({ description, children }) => (
-  <div className="row gy-2">
+  <div className="row gy-3">
     {description && <p className="mb-0">{description}</p>}
     {children}
   </div>
