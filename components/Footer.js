c77a0cfe3a87eb6a09d48d816083f63f1702992d
Kirk Douglas Jr <kirkodouglasjr@gmail.com>
2023-01-29 16:27:02 -0600

    added change language link to footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import { useTrans } from "../lib/trans";
+import { useRouter } from "next/router";
+import { track } from "../lib/track";
 
 const ProductHunt = () => (
   <a
@@ -18,6 +20,25 @@ const ProductHunt = () => (
 
 const Footer = () => {
   const { t } = useTrans();
+  const router = useRouter();
+  const localButtonLabel =
+    router.locale === "en"
+      ? "Cambiar idioma a Español"
+      : "Change Language to English";
+  const toggleLocale = async (e) => {
+    // switch to the other locale
+    e.preventDefault();
+    const nextLocale = router.locale === "en" ? "es" : "en";
+    track("nav.footer.locale", { locale: nextLocale });
+    await router.push(
+      { pathname: router.asPath, query: router.query },
+      router.asPath,
+      {
+        locale: nextLocale,
+      }
+    );
+  };
+
   return (
     <div className="container my-5 text-center">
       <p>
@@ -32,11 +53,20 @@ const Footer = () => {
           <i className="bi bi-instagram"></i> Instagram
         </Link>
       </p>
+
       <p>
-        <i className="bi bi-c-square"></i> Activazon 2023 |{" "}
-        <Link href="/legal" className="link">
+        <span className="me-3">
+          <i className="bi bi-c-square"></i> Activazon 2023
+        </span>
+        |
+        <Link href="/legal" className="link ms-3">
           {t("Terms of Use")}
-        </Link>{" "}
+        </Link>
+      </p>
+      <p>
+        <Link onClick={toggleLocale} href="#" className="link ms-3">
+          <i className="bi bi-translate"></i> {localButtonLabel}
+        </Link>
       </p>
     </div>
   );
