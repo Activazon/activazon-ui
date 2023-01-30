61816c0568e3aa30d9f10844094ed2571064acd1
Kirk Douglas Jr <kirkodouglasjr@gmail.com>
2023-01-29 20:39:26 -0600

    Update search.js

--- pages/search.js
+++ pages/search.js
@@ -90,7 +90,7 @@ export default function Home() {
             {isBusy && (
               <div className="w-100 text-center mt-5">
                 <div
-                  class="spinner-border"
+                  className="spinner-border"
                   style={{ width: "3rem", height: "3rem" }}
                   role="status"
                 ></div>
