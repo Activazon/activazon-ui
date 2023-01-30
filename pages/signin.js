e12d931a174462ba679d865999c8088be865acc9
Kirk Douglas Jr <kirkodouglasjr@gmail.com>
2023-01-29 20:38:47 -0600

    removed signup tmp redirect

--- pages/signin.js
+++ pages/signin.js
@@ -128,22 +128,3 @@ const Page = ({}) => {
 };
 
 export default Page;
-
-export async function getServerSideProps(context) {
-  const session = await getSession(context);
-
-  if (!session) {
-    return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-      },
-    };
-  }
-
-  return {
-    props: {
-      session,
-    },
-  };
-}
