61402e0c09658692b8c454a287876245ae59a72c
Kirk Douglas Jr <kirkodouglasjr@gmail.com>
2023-04-02 18:51:05 -0600

    improvements to cta a2hs widget or tile

--- components/A2hsCtaTile.js
+++ components/A2hsCtaTile.js
@@ -15,36 +15,56 @@ const A2hsCtaTile = ({}) => {
   };
 
   return (
-    <div className="card card-body tile tile-cta">
-      <div className="row text-center gy-3">
-        <div className="col-12">
-          <Image
-            src="/icons/push-notification.svg"
-            width={90}
-            height={90}
-            alt="Push Notification"
-          />
-        </div>
-        <div className="col-12">
-          <h2 className="tile-title">
-            {i("Stay alert and stay safe with Activazon")}
-          </h2>
-        </div>
-        <div className="col-12">
-          <p className="tile-description">
-            {i(
-              "Add Activazon to your home screen to get real-time notifications about crime in your area."
-            )}
-          </p>
-        </div>
-        <div className="col-12">
-          <button onClick={goToA2hs} className="btn btn-tile">
-            {i("Add To Home Screen")}
-          </button>
+    <div className="widget widget-primary pointer" onClick={goToA2hs}>
+      <div className="widget-list">
+        <div className="widget-list-item">
+          <div className="widget-list-item-image widget-list-item-image-small d-flex align-items-center justify-content-center">
+            <i class="bi bi-bell-fill display-5 animate-bell"></i>
+          </div>
+          <div className="widget-list-item-content">
+            <h6 className="widget-list-item-title">
+              {i("Stay alert and stay safe with Activazon")}
+            </h6>
+            <p className="widget-list-item-text widget-list-item-text-light">
+              {i(
+                "Add Activazon to your home screen to get real-time notifications about crime in your area."
+              )}
+            </p>
+            <button onClick={goToA2hs} className="btn btn-cta">
+              {i("Add To Home Screen")}
+            </button>
+          </div>
         </div>
       </div>
     </div>
   );
+
+  // return (
+  //   <div className="card card-body tile tile-cta">
+  //     <div className="row text-center gy-3">
+  //       <div className="col-12">
+
+  //       </div>
+  //       <div className="col-12">
+  //         <h2 className="tile-title">
+  //           {i("Stay alert and stay safe with Activazon")}
+  //         </h2>
+  //       </div>
+  //       <div className="col-12">
+  //         <p className="tile-description">
+  //           {i(
+  //             "Add Activazon to your home screen to get real-time notifications about crime in your area."
+  //           )}
+  //         </p>
+  //       </div>
+  //       <div className="col-12">
+  //         <button onClick={goToA2hs} className="btn btn-tile">
+  //           {i("Add To Home Screen")}
+  //         </button>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 };
 
 export default A2hsCtaTile;
