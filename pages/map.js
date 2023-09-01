b164155402461748500ff48462001f8cd965f649
Kirk Douglas Jr <kirkodouglasjr@gmail.com>
2023-08-31 21:35:35 -0600

    Update map.js

--- pages/map.js
+++ pages/map.js
@@ -73,7 +73,7 @@ export default function Page() {
 
   return (
     <>
-      <Head title={null} />
+      <Head title={"Beta Map"} />
       <body>
         <div className="page">
           <Nav backHref={null} />
