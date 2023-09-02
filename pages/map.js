2717399b38d7e9d19e1d4e2df1e55fb596512df6
Kirk Douglas Jr <kirkodouglasjr@gmail.com>
2023-09-02 12:22:45 -0600

    added code to add markers

--- pages/map.js
+++ pages/map.js
@@ -7,6 +7,8 @@ import Content from "components/Content/Content";
 import ContentGroup from "components/Content/ContentGroup";
 import { ItemList, Item } from "components/ItemList";
 
+import "mapbox-gl/dist/mapbox-gl.css";
+
 import { useTrans } from "lib/trans";
 
 import { useTrackOnce, track } from "lib/track";
@@ -71,6 +73,29 @@ export default function Page() {
     } else {
       setCity(results.closest_city);
       setAreas([...results.areas]);
+
+      // reset previous markers
+      [...map.current._markers].forEach((marker) => marker.remove());
+
+      // set markers
+      [...results.areas].forEach((area) => {
+        const el = document.createElement("div");
+        el.className = "tw-bg-blue-bright tw-rounded-full";
+        el.style.width = "20px";
+        el.style.height = "20px";
+
+        el.addEventListener("click", () => {
+          window.location.href = area.slug_path;
+        });
+
+        // add marker to map
+        new mapboxgl.Marker(el)
+          .setLngLat([
+            area.location_coordinates[1],
+            area.location_coordinates[0],
+          ])
+          .addTo(map.current);
+      });
     }
     setIsSearching(false);
   };
@@ -83,13 +108,7 @@ export default function Page() {
           <Nav backHref={null} />
           <Content>
             <SearchBar onPlaceSelect={onPlaceSelect} />
-            {/* beta disclaimer */}
-            <div className="tw-w-full tw-rounded-2xl tw-overflow-hidden tw-bg-yellow-200 tw-p-3">
-              <p className="tw-m-0 tw-p-0 tw-text-sm">
-                This is currently a beta feature and may still contain some
-                issues. Please provide your feedback via instagram @activazon
-              </p>
-            </div>
+
             {/* map */}
             <div className="tw-w-full tw-h-[500px] tw-rounded-2xl tw-overflow-hidden tw-bg-blue-dark">
               <div
