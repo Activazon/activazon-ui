43e5a569131b3434f801816ca9a9f21aeffcf1b5
Kirk Douglas Jr <kirkodouglasjr@gmail.com>
2023-10-20 00:21:53 -0600

    fixed pwa issue

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa")({
-  dest: "public/pwa",
-  register: false,
-  skipWaiting: true,
-  // importScripts: ["/worker/index.js"],
-
-  // disable: process.env.NODE_ENV === "development",
-});
 
 const nextConfig = {};
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
