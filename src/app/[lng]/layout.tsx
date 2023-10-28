2b2a852d8dc0f9cc76845bcc443e086d2af3f6d2
Kirk Douglas Jr <kirkodouglasjr@gmail.com>
2023-10-27 22:39:03 -0600

    prevent scaling

--- src/app/[lng]/layout.tsx
+++ src/app/[lng]/layout.tsx
@@ -59,6 +59,8 @@ export const metadata: Metadata = {
   },
   category: "news",
   classification: "AI",
+  viewport:
+    "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
 };
 
 export default function RootLayout({
