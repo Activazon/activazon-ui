22379fbeead00286b664770de4d8440a0be71d9e
Kirk Douglas Jr <kirkodouglasjr@gmail.com>
2023-11-26 18:09:28 -0500

    notifications ui design improvements

--- src/components/ItemList/ItemAttentionWrapper.tsx
+++ src/components/ItemList/ItemAttentionWrapper.tsx
@@ -7,11 +7,12 @@ const ItemAttentionWrapper = ({
   children: React.ReactNode;
   showAttention: boolean;
 }) => {
-  if (!showAttention) {
-    return <>{children}</>;
-  }
   return (
-    <div className="tw-bg-blue-light/40 tw-mx-[-0.75rem] tw-mt-[-0.75rem] tw-px-3 tw-py-3 tw-border-b tw-border-blue-light">
+    <div
+      className={`tw-p-2 tw-rounded-xl ${
+        showAttention ? "tw-bg-blue-light/30" : ""
+      }`}
+    >
       {children}
     </div>
   );
