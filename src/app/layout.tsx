893632290d135f1fc812cab7adc5c744804789c6
Kirk Douglas Jr <kirkodouglasjr@gmail.com>
2023-11-27 12:45:14 -0500

    added fullstory

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import "./globals.css";
 
+import { init as initFullStory } from "@fullstory/browser";
+
+initFullStory({ orgId: "o-1GXTF6-na1" });
+
 export default function RootLayout({
   children,
 }: {
