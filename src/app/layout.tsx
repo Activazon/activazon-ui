3ab246f82d05cd28dbf05563ef1af05fd8f5e562
Kirk Douglas Jr <kirkodouglasjr@gmail.com>
2023-11-27 13:02:11 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,16 @@
+"use client";
+import { useEffect } from "react";
 import "./globals.css";
 
 import { init as initFullStory } from "@fullstory/browser";
-
-initFullStory({ orgId: "o-1GXTF6-na1" });
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  useEffect(() => {
+    initFullStory({ orgId: "o-1GXTF6-na1" });
+  }, []);
   return children;
 }
