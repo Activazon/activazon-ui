ed92e9b6b84009db58fe13123bf43c1335f4dfb3
Kirk Douglas Jr <kirkodouglasjr@gmail.com>
2023-12-15 11:58:11 -0600

    Update SearchForPlaces.tsx

--- src/components/SearchForPlaces.tsx
+++ src/components/SearchForPlaces.tsx
@@ -4,11 +4,13 @@ import Link from "next/link";
 const SearchForPlaces = () => {
   const { t } = useDictionary();
   return (
-    <div className="tw-w-full tw-bg-blue-light-2 tw-rounded-lg tw-p-5 tw-border-2 tw-border-blue-light">
-      <p className="tw-text-blue-dark tw-mb-4">{t("common:subscribe_cta")}</p>
+    <div className="tw-w-full tw-bg-blue-light tw-rounded-lg tw-p-5 tw-border-2 tw-border-blue-light">
+      <p className="tw-text-white tw-text-lg tw-mb-4">
+        {t("common:subscribe_cta")}
+      </p>
       <Link
         href="/search"
-        className="tw-bg-blue-light tw-py-2 tw-px-4 tw-rounded-md tw-text-white"
+        className="tw-border tw-border-white tw-py-2 tw-px-4 tw-rounded-md tw-text-white"
       >
         {t("common:search_placeholder")}
       </Link>
