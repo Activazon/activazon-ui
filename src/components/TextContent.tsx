6de15462c4007e534f37690d94809b06b2724ec3
Kirk Douglas Jr <kirkodouglasjr@gmail.com>
2023-12-15 12:58:56 -0600

    Update TextContent.tsx

--- src/components/TextContent.tsx
+++ src/components/TextContent.tsx
@@ -9,7 +9,7 @@ export const TextContent = ({ children, title, pulse }: TextContent) => {
     <div className="tw-p-4 tw-bg-slate-100 tw-border tw-border-slate-200 tw-rounded-2xl tw-flex tw-flex-col tw-gap-2">
       <p className=" tw-font-semibold tw-text-lg">{title}</p>
       {!pulse && (
-        <div className="tw-font-normal tw-text-gray-dark">{children}</div>
+        <div className="tw-font-medium tw-text-gray-dark">{children}</div>
       )}
       {pulse && (
         <div className="tw-grid tw-grid-cols-7 tw-gap-2 tw-animate-pulse tw-flex-grow">
