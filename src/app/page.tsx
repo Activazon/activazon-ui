0054a53e4ac6f7da49433f97fc40ae38d9517a20
Kirk Douglas Jr <kirkodouglasjr@gmail.com>
2025-02-24 13:19:30 -0600

    added gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { Fragment, ReactNode } from "react";
 import { HugeiconsIcon } from "@hugeicons/react";
 import {
   House03Icon as HouseIcon,
@@ -7,7 +7,6 @@ import {
   TrafficLightIcon as TrafficIcon,
   CourtLawIcon as LawIcon,
 } from "@hugeicons/core-free-icons";
-import { Waitlist } from "@clerk/nextjs";
 import JoinWaitListButton from "./JoinWaitListButton";
 
 const Banner = () => (
@@ -15,7 +14,7 @@ const Banner = () => (
     <img
       src="/logo.svg"
       alt="Activazon Logo"
-      className="w-full max-w-[250px] md:max-w-[300px"
+      className="w-full max-w-[250px] md:max-w-[300px] drop-shadow-xl"
     />
     <p className="md:text-lg font-semibold">
       Community Intelligence for Decision-Makers
@@ -47,51 +46,54 @@ const Icon = ({ icon }: { icon: any }) => (
 
 export default function Home() {
   return (
-    <div className="w-screen min-h-screen flex justify-center items-center">
-      <div className="w-full max-w-[500px] flex flex-col justify-end gap-5">
-        <Banner />
+    <Fragment>
+      <div className="mesh-gradient-bg " />
+      <div className="w-screen min-h-screen flex justify-center items-center">
+        <div className="w-full max-w-[500px] flex flex-col justify-end gap-5">
+          <Banner />
 
-        <div className="px-4">
-          <p className="text-white/90">
-            Get AI-powered insights on any aspect of your community,
-            including...
-          </p>
-        </div>
+          <div className="px-4">
+            <p className="text-white/90">
+              Get AI-powered insights on any aspect of your community,
+              including...
+            </p>
+          </div>
 
-        <div className="w-full flex flex-col bg-white/[0.03] md:rounded-2xl border-t border-b border-white/5 md:border overflow-hidden">
-          <UseCase
-            icon={<Icon icon={HouseIcon} />}
-            title="Real Estate Trends"
-            description="Property values, market shifts & investment opportunities."
-          />
-          <UseCase
-            icon={<Icon icon={StoreIcon} />}
-            title="Business Openings & Closures"
-            description="Stay ahead of market shifts & local economy trends."
-          />
-          <UseCase
-            icon={<Icon icon={CrimeIcon} />}
-            title="Crime & Safety Reports"
-            description="Understand community risks & safety changes."
-          />
-          <UseCase
-            icon={<Icon icon={TrafficIcon} />}
-            title="Infrastructure & Transit Changes"
-            description="Be informed about road expansions & city planning."
-          />
-          <UseCase
-            icon={<Icon icon={LawIcon} />}
-            title="Regulatory & Policy Updates"
-            description="See how new laws impact property & businesses."
-          />
+          <div className="w-full flex flex-col bg-white/[0.03] md:rounded-2xl border-t border-b border-white/5 md:border overflow-hidden">
+            <UseCase
+              icon={<Icon icon={HouseIcon} />}
+              title="Real Estate Trends"
+              description="Property values, market shifts & investment opportunities."
+            />
+            <UseCase
+              icon={<Icon icon={StoreIcon} />}
+              title="Business Openings & Closures"
+              description="Stay ahead of market shifts & local economy trends."
+            />
+            <UseCase
+              icon={<Icon icon={CrimeIcon} />}
+              title="Crime & Safety Reports"
+              description="Understand community risks & safety changes."
+            />
+            <UseCase
+              icon={<Icon icon={TrafficIcon} />}
+              title="Infrastructure & Transit Changes"
+              description="Be informed about road expansions & city planning."
+            />
+            <UseCase
+              icon={<Icon icon={LawIcon} />}
+              title="Regulatory & Policy Updates"
+              description="See how new laws impact property & businesses."
+            />
+          </div>
+          <JoinWaitListButton />
+          <footer className="w-full mt-5">
+            <p className="text-center text-sm text-white/70">
+              &copy; Activazon 2025
+            </p>
+          </footer>
         </div>
-        <JoinWaitListButton />
-        <footer className="w-full mt-5">
-          <p className="text-center text-sm text-white/70">
-            &copy; Activazon 2025
-          </p>
-        </footer>
       </div>
-    </div>
+    </Fragment>
   );
 }
