8224fb90f540077079539253ec800c59a5156092
Kirk Douglas Jr <kirkodouglasjr@gmail.com>
2025-02-25 00:43:19 -0600

    wip

--- src/app/blog/(blog-content)/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <div className="px-4 md:px-0 w-full max-w-[1000px] mx-auto">{children}</div>
-  );
-}
