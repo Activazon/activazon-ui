d87b3e7c43fce9916409951e7f602f72ec5ff0a2
Kirk Douglas Jr <kirkodouglasjr@gmail.com>
2025-02-25 00:56:11 -0600

    Update page.tsx

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -41,15 +41,21 @@ export default async function RemoteMdxPage({
 
   return (
     <div className="px-4 md:px-0 w-full max-w-[1000px] mx-auto">
+      {/* Title */}
       <div className="mt-12 mb-6">
         <h1 className="text-4xl md:text-5xl font-bold tracking-tight text-white leading-tight">
           {metadata.title}
         </h1>
-        <p className="text-sm text-white/50">
-          {format(new Date(metadata.date), "MMMM d, yyyy")}
-        </p>
-        <p className="text-sm text-white/50">{metadata.author}</p>
+
+        {/* Metadata Section */}
+        <div className="flex items-center text-white/80 mt-3 space-x-3 font-medium">
+          <p>{format(new Date(metadata.date), "MMMM d, yyyy")}</p>
+          <span className="h-4 w-[1px] bg-white/20"></span> {/* Divider */}
+          <p>{metadata.author}</p>
+        </div>
       </div>
+
+      {/* MDX Content */}
       <MDXRemote components={MDX_COMPONENTS} source={content} />
     </div>
   );
