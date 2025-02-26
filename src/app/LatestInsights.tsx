3283bd9c0e21533512656f2c003761bff4b00de8
Kirk Douglas Jr <kirkodouglasjr@gmail.com>
2025-02-26 16:39:36 -0600

    added another post

--- src/app/LatestInsights.tsx
+++ src/app/LatestInsights.tsx
@@ -2,28 +2,31 @@ import { MdxFrontMatter } from "@/app/blog/lib/mdx";
 import Link from "next/link";
 
 const LatestInsights = ({ posts }: { posts: MdxFrontMatter[] }) => {
-  const post = posts?.[0];
+  const latestPosts = posts?.slice(0, 3); // Get the first 3 posts
 
-  if (!post) {
+  if (!latestPosts || latestPosts.length === 0) {
     return null;
   }
+
   return (
     <section className="w-full mt-10 px-4 md:px-0">
       <h2 className="text-2xl font-semibold text-white">Latest Insights</h2>
       <div className="mt-4 space-y-3">
-        <Link
-          key={post.slug}
-          href={`/blog/${post.slug}`}
-          className="group block"
-        >
-          <div className="p-4 bg-white/5 hover:bg-[#00A3FF]/20 rounded-lg transition duration-200 border border-white/5">
-            <div className="mb-2 text-white group-hover:text-[#00A3FF] transition-all">
-              <h3 className=" inline text-lg font-medium">{post.title}</h3>
-              <p className="ml-1.5 inline opacity-80">{post.sub_title}</p>
+        {latestPosts.map((post) => (
+          <Link
+            key={post.slug}
+            href={`/blog/${post.slug}`}
+            className="group block"
+          >
+            <div className="p-4 bg-white/5 hover:bg-[#00A3FF]/20 rounded-lg transition duration-200 border border-white/5">
+              <div className="mb-2 text-white group-hover:text-[#00A3FF] transition-all">
+                <h3 className="inline text-lg font-medium">{post.title}</h3>
+                <p className="ml-1.5 inline opacity-80">{post.sub_title}</p>
+              </div>
+              <p className="text-sm text-white/60">{post.excerpt}</p>
             </div>
-            <p className="text-sm text-white/60">{post.excerpt}</p>
-          </div>
-        </Link>
+          </Link>
+        ))}
       </div>
       <Link href="/blog">
         <p className="text-[#00A3FF] underline decoration-[#00A3FF]/50 hover:decoration-[#00A3FF] transition-all duration-200 underline-offset-4 text-sm mt-3">
